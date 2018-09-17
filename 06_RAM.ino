de9bc6128259d21a09bc2d84ed5cd9e92ca221e7
ProjetosIoT <43098826+projetosiot@users.noreply.github.com>
2018-09-17 20:08:42 -0300

    Add files via upload

---
+++ 06_RAM.ino
@@ -0,0 +1,20 @@
+byte memoria;
+int comando;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  if (Serial.available()>0)
+  {
+
+    comando = Serial.parseInt();
+
+    if (comando==1) memoria = Serial.parseInt();
+    if (comando==2) Serial.println(memoria);
+    
+  }
+
+}
