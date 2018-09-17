c0a920e6909b5a061974f1e64f87004649cd0918
ProjetosIoT <43098826+projetosiot@users.noreply.github.com>
2018-09-17 20:09:07 -0300

    Add files via upload

---
+++ 07_EEPROM.ino
@@ -0,0 +1,30 @@
+#include <EEPROM.h>
+
+byte memoria;
+int comando;
+
+void setup() {
+  Serial.begin(115200);
+  EEPROM.begin(1);
+}
+
+void loop() {
+
+  if (Serial.available()>0)
+  {
+    comando = Serial.parseInt();
+
+    if (comando==1)
+    {
+      memoria = Serial.parseInt();
+      EEPROM.write(0, memoria);
+      EEPROM.commit();
+    }
+    if (comando==2){
+      memoria = EEPROM.read(0);
+      Serial.println(memoria);
+    }
+    
+  }
+
+}
