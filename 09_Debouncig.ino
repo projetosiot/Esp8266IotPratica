8d495445985a61194da3c090cc1bd2b4a4b5d130
ProjetosIoT <43098826+projetosiot@users.noreply.github.com>
2018-09-17 22:51:49 -0300

    Add files via upload

---
+++ 09_Debouncig.ino
@@ -0,0 +1,52 @@
+
+/* 
+ DESCRIPTION
+ ====================
+ Simple example of the Bounce library that switches the debug LED when a button is pressed.
+ */
+// Include the Bounce2 library found here :
+// https://github.com/thomasfredericks/Bounce2
+
+#include <Bounce2.h>
+
+#define BUTTON_PIN D3
+#define LED_PIN D4
+
+int ledState = LOW;
+
+// Instantiate a Bounce object
+Bounce debouncer = Bounce(); 
+
+void setup() {
+
+  // Setup the button with an internal pull-up :
+  pinMode(BUTTON_PIN,INPUT_PULLUP);
+
+  // After setting up the button, setup the Bounce instance :
+  debouncer.attach(BUTTON_PIN);
+  debouncer.interval(5); // interval in ms
+
+  //Setup the LED :
+  pinMode(LED_PIN,OUTPUT);
+
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // Update the Bounce instance :
+  debouncer.update();
+
+  // Get the updated value :
+  int value = debouncer.read();
+
+  Serial.println(value);
+
+ if (debouncer.fell()){  
+    ledState = !ledState;
+    digitalWrite(LED_PIN,ledState);
+ }
+
+}
+
+
